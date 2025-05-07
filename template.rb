f641c7d9d2b84ba11fd4806926fcb6384d2e1fad
arthurariza <tucaariza@gmail.com>
2025-05-06 23:28:57 -0300

    allow .env.template

--- template.rb
+++ template.rb
@@ -86,6 +86,9 @@ after_bundle do
   run "rm railsrc"
   run "rm template.rb"
   run "rm bin/rails-new"
+  insert_into_file ".gitignore" do
+    "!.env.template"
+  end
   git_add_and_commit "Cleanup"
 
   run "bundle exec rubocop -a"
